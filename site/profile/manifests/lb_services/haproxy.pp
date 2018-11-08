b940e9e16152d5064ae744c53e1684de503e201c
ginodambrosio <44186268+ginodambrosio@users.noreply.github.com>
2018-11-08 16:03:02 +1030

    Create haproxy.pp

---
+++ site/profile/manifests/lb_services/haproxy.pp
@@ -0,0 +1,26 @@
+class puppet::proxy {
+
+  include ::haproxy
+
+  haproxy::listen { 'puppet':
+    collect_exported => false,
+    ipaddress        => $::ipaddress,
+    ports            => '8140',
+  }
+
+  haproxy::balancermember { 'compile master 1':
+    listening_service => 'puppet',
+    server_names      => 'compile-master-0.local',
+    ipaddresses       => '192.168.2.5',
+    ports             => '8140',
+    options           => 'check',
+  }
+
+  haproxy::balancermember { 'compile master 2':
+    listening_service => 'puppet',
+    server_names      => 'compile-master-1.local',
+    ipaddresses       => '192.168.2.6',
+    ports             => '8140',
+    options           => 'check'
+  }
+}
