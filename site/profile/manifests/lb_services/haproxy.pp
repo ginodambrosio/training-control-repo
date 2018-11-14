40df2b373e49afb6ffb4cfd3271ffb1c2e7103f2
ginodambrosio <44186268+ginodambrosio@users.noreply.github.com>
2018-11-14 22:38:42 +1100

    Hard coded  ipaddress        => '192.168.2.7'

--- site/profile/manifests/lb_services/haproxy.pp
+++ site/profile/manifests/lb_services/haproxy.pp
@@ -4,7 +4,7 @@ class profile::lb_services::haproxy {
 
   haproxy::listen { 'puppet':
     collect_exported => false,
-    ipaddress        => $facts['networking']['ip'],
+    ipaddress        => '192.168.2.7',
     ports            => '8140',
   }
 
