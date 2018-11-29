0ae710a4e2e091c93126b4c8218f9c43d26d4b5f
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-11-29 11:16:32 +0000

    Updated ca_signer.pp

--- site/profile/manifests/ca_signer.pp
+++ site/profile/manifests/ca_signer.pp
@@ -1,3 +1,12 @@
 class profile::ca_signer {
 
+include ::autosign
+
+ini_setting { 'policy-based autosigning':
+  setting => 'autosign',
+  path    => "${confdir}/puppet.conf",
+  section => 'master',
+  value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+  notify  => Service['pe-puppetserver'],
+  }
 }
