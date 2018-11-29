7ed3ec77c414d27c9f671b75d83307ce6160a3c0
Ubuntu <ubuntu@slicejump.platform9.puppet.net>
2018-11-29 09:31:45 +0000

    Created autosign profile manifest

---
+++ site/profile/manifests/autosign.pp
@@ -0,0 +1,22 @@
+class profile::autosign {
+ini_setting { 'policy-based autosigning':
+  setting => 'autosign',
+  path    => "${confdir}/puppet.conf",
+  section => 'master',
+  value   => '/opt/puppetlabs/puppet/bin/autosign-validator',
+  notify  => Service['pe-puppetserver'],
+}
+
+class { ::autosign:
+  ensure => 'latest',
+  config => {
+    'general' => {
+      'loglevel' => 'INFO',
+    },
+    'jwt_token' => {
+      'secret'   => 'puppetlabs'
+      'validity' => '10800',
+    }
+  },
+}
+}
