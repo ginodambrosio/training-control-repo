732af1f388b117d4186a3a4b8ca6959e8736aefe
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-14 11:12:16 +0000

    Added puppetlabs-registry module to disable IE enhanced security config

--- site/profile/manifests/windows/basics3.pp
+++ site/profile/manifests/windows/basics3.pp
@@ -7,21 +7,30 @@ class profile::windows::basics3 {
         membership      => inclusive,
         password        => 'Puppetlabs123',
     }
+
   group{'legends':
         name            => 'Legends',
         ensure          => present,
         members         => ['Gino DAmbrosio'],
     }
+
   file { 'c:/Users/gino':
   ensure => 'directory',
 #  mode   => '0660',
 #  owner  => 'Gino DAmbrosio',
 #  group  => 'Legends',
     }
+
   acl { 'c:/Users/gino':
     permissions => [
      { identity => 'Gino DAmbrosio', rights => ['full'] },
      { identity => 'Legends', rights => ['read'] }
      ],
+    }
+
+  registry_value { 'HKLM\Software\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+    ensure => present,
+    type   => dword,
+    data   => 0,
     }
 }
