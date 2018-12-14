4f5dd9bb8b4b5a453109f9d6295666918d0bacce
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-14 11:21:51 +0000

    Added Windows Shutdown Event Tracker via registry

--- site/profile/manifests/windows/basics3.pp
+++ site/profile/manifests/windows/basics3.pp
@@ -33,4 +33,16 @@ class profile::windows::basics3 {
     type   => dword,
     data   => 0,
     }
+
+  registry_value { 'HKLM\Software\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
+    ensure => present,
+    type   => dword,
+    data   => 0,
+    }
+
+  registry_value { 'HKLM\Software\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
+    ensure => present,
+    type   => dword,
+    data   => 1,
+    }
 }
