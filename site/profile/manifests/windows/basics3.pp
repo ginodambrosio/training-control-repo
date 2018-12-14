4a5d840334e8d01d2471a03a16ec6f99719eda92
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-14 11:29:39 +0000

    Enabled IE enhanced security config via registry

--- site/profile/manifests/windows/basics3.pp
+++ site/profile/manifests/windows/basics3.pp
@@ -31,13 +31,13 @@ class profile::windows::basics3 {
   registry_value { 'HKLM\Software\Microsoft\Active Setup\Installed Components\{A509B1A7-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => dword,
-    data   => 0,
+    data   => 1,
     }
 
   registry_value { 'HKLM\Software\Microsoft\Active Setup\Installed Components\{A509B1A8-37EF-4b3f-8CFC-4F3A74704073}\IsInstalled':
     ensure => present,
     type   => dword,
-    data   => 0,
+    data   => 1,
     }
 
   registry_value { 'HKLM\Software\Microsoft\Windows\CurrentVersion\Reliability\ShutdownReasonUI':
