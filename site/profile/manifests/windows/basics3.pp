6c6fa4a2992f4eb4bc0e13d52cc0d082e912c758
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-18 12:02:55 +0000

    Stage and install 7zip via msi rather than choclatey. Plus extraxt puppet help tarball from web

--- site/profile/manifests/windows/basics3.pp
+++ site/profile/manifests/windows/basics3.pp
@@ -116,6 +116,7 @@ class profile::windows::basics3 {
   archive { 'C:/Users/gino/putty-0.70.tar.gz':
     source        => 'http://mirror.internode.on.net/pub/putty/0.70/putty-0.70.tar.gz',
     extract       => true,
+    extract_command => 'tar xfz %s',
     extract_path  => "C:/Users/gino",
     creates       => "C:/Users/gino/putty" #directory inside tgz
   }
