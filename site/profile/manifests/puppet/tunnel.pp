d78cffb45f52effa1fd2343156150116bf13c0dd
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-24 07:55:08 +0000

    Remove webhook resource

--- site/profile/manifests/puppet/tunnel.pp
+++ site/profile/manifests/puppet/tunnel.pp
@@ -1,8 +1,7 @@
 class { 'ngrok':
   authtoken       => '84fAzMWrconh6dpfd1kRb_3DBPbRkWkNPYjiBNQUYYQ',
 }
-
-ngrok::tunnel { 'webhook':
-  proto => 'tcp',
-  addr  => '8170',
-}
+#ngrok::tunnel { 'webhook':
+  #proto => 'tcp',
+  #addr  => '8170',
+#}
