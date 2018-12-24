1a00dbbb49b12b2a38c86a6a9f2bbc28d0423be2
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-24 08:43:10 +0000

    Changed syntax to class ngrok

--- site/profile/manifests/puppet/tunnel.pp
+++ site/profile/manifests/puppet/tunnel.pp
@@ -1,8 +1,8 @@
 class profile::puppet::tunnel {
-  #class { 'ngrok':
-    #authtoken       => '84fAzMWrconh6dpfd1kRb_3DBPbRkWkNPYjiBNQUYYQ',
-  #}
-  include ngrok
+  class { 'ngrok':
+    authtoken       => '84fAzMWrconh6dpfd1kRb_3DBPbRkWkNPYjiBNQUYYQ',
+  }
+ # include ngrok
   ngrok::tunnel { 'webhook':
     proto => 'tcp',
     addr  => '8170',
