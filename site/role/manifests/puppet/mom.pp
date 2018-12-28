394da28d995921b825fa10a12fdaf95cff6110c7
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2018-12-28 05:21:55 +0000

    Added global myvar variable and notice debug statement

--- site/role/manifests/puppet/mom.pp
+++ site/role/manifests/puppet/mom.pp
@@ -1,5 +1,5 @@
 class role::puppet::mom {
   include profile::puppet::ca_signer
   include profile::puppet::tunnel
-  notify{"The globuvar value is: ${globuvar}": }
+  notify{"The myvar value is: ${myvar}": }
 }
