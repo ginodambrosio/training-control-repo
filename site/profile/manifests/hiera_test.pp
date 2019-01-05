4f76b942e401ced991653a318a1e76f8cc44a67d
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2019-01-05 07:27:36 +0000

    Modified to suit Windows

--- site/profile/manifests/hiera_test.pp
+++ site/profile/manifests/hiera_test.pp
@@ -13,19 +13,19 @@ class profile::hiera_test (
     }
   default: {
     fail('Unsupported operating system!')
+    }
   }
-  }
-#  file { '$temp/hiera_test.txt':
-#    ensure  => file,
-#    content => @("END"),
-#               Data from profile::hiera_test
-#               -----
-#               profile::hiera_test::myvar= ${myvar}
-#               profile::hiera_test::testvar= ${testvar}
-#               profile::hiera_test::environment= ${::environment}
-#               |END
+  file { '$temp/hiera_test.txt':
+    ensure  => file,
+    content => @("END"),
+               Data from profile::hiera_test
+               -----
+               profile::hiera_test::myvar= ${myvar}
+               profile::hiera_test::testvar= ${testvar}
+               profile::hiera_test::environment= ${::environment}
+               |END
 #    owner   => root,
 #    mode    => '0644',
-#  }
-#  notify{"myvar: ${myvar} | testvar: ${testvar} | environment: $::environment": }
+  }
+  notify{"myvar: ${myvar} | testvar: ${testvar} | environment: $::environment": }
 }
