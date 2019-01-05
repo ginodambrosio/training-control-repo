a5dfd9b06a64189d51498cc114449e50dad7ef75
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2019-01-05 04:55:02 +0000

    Removed profile hiera_test

--- site/role/manifests/puppet/mom.pp
+++ site/role/manifests/puppet/mom.pp
@@ -1,5 +1,4 @@
 class role::puppet::mom {
-  include profile::hiera_test
   include profile::puppet::ca_signer
   include profile::puppet::tunnel
 #  notify{"myvar is: ${myvar} and environment is: $::environment": }
