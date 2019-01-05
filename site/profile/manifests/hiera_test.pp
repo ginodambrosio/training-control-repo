972d72d10435d20ce236de7fb03f885314eb2c2c
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2019-01-05 04:46:37 +0000

    Added base role and hiera_test profile

---
+++ site/profile/manifests/hiera_test.pp
@@ -0,0 +1,16 @@
+class profile::hiera_test (
+  Optional[String[1]] $testvar = undef,
+) {
+  file { '/tmp/hiera_test.txt':
+    ensure  => file,
+    content => @("END"),
+               Data from profile::hiera_test
+               -----
+               profile::hiera_test::testvar: ${testvar}
+               profile::hiera_test::environment: ${::environment}
+               |END
+    owner   => root,
+    mode    => '0644',
+  }
+  notify{"myvar: ${myvar} testvar: ${testvar} environment: $::environment": }
+}
---
+++ site/role/manifests/base.pp
@@ -0,0 +1,4 @@
+class role::base { 
+  include profile::hiera_test
+  notify{"myvar is: ${myvar} and environment is: $::environment": }
+}
