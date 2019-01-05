3db0824d8fca5c237e19452fb33b42117084c0db
Gino D'Ambrosio <gino.dambrosio@puppet.com>
2019-01-05 07:20:14 +0000

    Modified to suit Windows

--- site/profile/manifests/hiera_test.pp
+++ site/profile/manifests/hiera_test.pp
@@ -4,27 +4,27 @@ class profile::hiera_test (
   # OS-specific
   case $facts['kernel'] {
     'windows': {
-      $temp='C:/Temp'
-      notify {"Got to windows":}
+      $temp = 'C:/Temp'
+      notify {"Got to windows ${temp}":}
     }
     'Linux': {
-      $temp='/tmp'
-      notify {"Got to linux":}
+      $temp = '/tmp'
+      notify {"Got to linux ${temp}":}
     }
   default: {
     fail('Unsupported operating system!')
   }
-  file { '$temp/hiera_test.txt':
-    ensure  => file,
-    content => @("END"),
-               Data from profile::hiera_test
-               -----
-               profile::hiera_test::myvar= ${myvar}
-               profile::hiera_test::testvar= ${testvar}
-               profile::hiera_test::environment= ${::environment}
-               |END
-    owner   => root,
-    mode    => '0644',
-  }
-  notify{"myvar: ${myvar} | testvar: ${testvar} | environment: $::environment": }
+#  file { '$temp/hiera_test.txt':
+#    ensure  => file,
+#    content => @("END"),
+#               Data from profile::hiera_test
+#               -----
+#               profile::hiera_test::myvar= ${myvar}
+#               profile::hiera_test::testvar= ${testvar}
+#               profile::hiera_test::environment= ${::environment}
+#               |END
+#    owner   => root,
+#    mode    => '0644',
+#  }
+#  notify{"myvar: ${myvar} | testvar: ${testvar} | environment: $::environment": }
 }
